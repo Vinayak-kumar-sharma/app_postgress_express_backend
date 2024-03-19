ace1f890be95bb430b4f5db55a747c5f108c27b3
Vinayak-kumar-sharma <vinayaksharma07@yahoo.com>
2024-03-19 11:24:53 +0530

    Please Sir Please

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import session from 'express-session'; 
-import Router from './auth.js'; 
+import Router from './routes/auth.js'; 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
